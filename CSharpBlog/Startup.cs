696b1f5c2d05f1c086467d837ebef0c88d8dbdc8
Krasimir <Krasimir@DESKTOP-HLR2TLM>
2016-08-22 12:12:32 +0300

    Add project files.

---
+++ CSharpBlog/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CSharpBlog.Startup))]
+namespace CSharpBlog
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
