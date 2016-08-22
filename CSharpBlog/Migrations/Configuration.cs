8e23741034618213ab7e9c952b88d687670a7a43
Krasimir <Krasimir@DESKTOP-HLR2TLM>
2016-08-22 16:40:31 +0300

    Enabled automatic migrations, seed method that creates administrator account and initialization in global.asax

---
+++ CSharpBlog/Migrations/Configuration.cs
@@ -0,0 +1,43 @@
+namespace CSharpBlog.Migrations
+{
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Models;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<CSharpBlog.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
+        }
+
+        protected override void Seed(CSharpBlog.Models.ApplicationDbContext context)
+        {
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+
+            if (!roleManager.RoleExists("Administrator"))
+            {
+                roleManager.Create(new IdentityRole("Administrator"));
+            }
+            if (userManager.FindByEmail("administrator@csharpblog.bg")==null)
+            {
+                var user = new ApplicationUser
+                {
+                    UserName = "administrator@csharpblog.bg",
+                    Email = "administrator@csharpblog.bg"
+                };
+                IdentityResult userResult = userManager.Create(user, "administrator");
+                if (userResult.Succeeded)
+                {
+                    userManager.AddToRole(user.Id, "Administrator");
+                }
+            }
+        }
+    }
+}
